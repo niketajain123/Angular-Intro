f0eaca9c3b076478ac5080f46fb735e17a63453f
niketajain123 <niketajain236@gmail.com>
2025-03-06 13:18:51 +0530

    [Niketa Jain]Add. uc5 name valiadtion while binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,22 @@ export class AppComponent {
   imgUrl = '../assets/BL-logo.png';
   url = 'https://www.bridgelabz.com';
   userName: string = '';
+  nameError: string = '';
+
   ngOnInit() {
     this.title = 'Hello from BridgeLabz';
   }
   onClick($event: any) {
     console.log('Save button is clicked!', $event);
     window.open(this.url, '_blank');
+  }
+  onInput($event: any) {
+    console.log('Change Event Occured!', $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = '';
+      return;
+    }
+    this.nameError = 'Name is Incorrect!';
   }
 }
