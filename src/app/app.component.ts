fc50f3e57cef3a00bd96f69c09295badb424aaa6
niketajain123 <niketajain236@gmail.com>
2025-03-06 11:55:51 +0530

    [Niketa Jain]Add. uc1 hello program added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hello-app';
+  title = 'angular-intro';
+
+  ngOnInit() {
+    this.title = 'Hello from BridgeLabz';
+  }
 }
